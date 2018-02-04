9d4cbd7713a10183a7751aa0322c8873cc58eea4
Stef <stedeflo@hotmail.com>
2018-02-04 16:42:37 +1100

    Unity Project

---
+++ Progeny/Assets/Scripts/FaceManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceManager : MonoBehaviour {
+
+    List<GameObject> faces;
+    public GameObject face_Neutral;
+    public GameObject face_Happy;
+    public GameObject face_Angry;
+
+
+
+    public enum FaceState { Neutral, Happy, Angry, }
+
+    // Use this for initialization
+    void Start()
+    {
+        faces = new List<GameObject>();
+        faces.Add(face_Neutral);
+        faces.Add(face_Happy);
+        faces.Add(face_Angry);
+
+        SetFaceState(FaceState.Neutral);
+    }
+
+    public void ButtonFaceSetter(string facetype)
+    {        
+        SetFaceState((FaceState)Enum.Parse(typeof(FaceState),facetype));
+    }
+
+	public void SetFaceState(FaceState faceState)
+    {
+        switch (faceState)
+        {
+            case FaceState.Neutral:
+                StartCoroutine(SetFaceNeutral());
+                break;
+
+            case FaceState.Happy:
+                StartCoroutine(SetFaceHappy());
+                break;
+
+            case FaceState.Angry:
+                StartCoroutine(SetFaceAngry());
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    IEnumerator SetFaceNeutral()
+    {
+        foreach (GameObject faceType in faces)
+        {
+            faceType.SetActive(false);
+        }
+        face_Neutral.SetActive(true);
+
+        yield return new WaitForSeconds(.1f);
+        
+
+    }
+
+    IEnumerator SetFaceHappy()
+    {
+        foreach (GameObject faceType in faces)
+        {
+            faceType.SetActive(false);
+        }
+        face_Happy.SetActive(true);
+
+        yield return new WaitForSeconds(.1f);
+
+    }
+
+    IEnumerator SetFaceAngry()
+    {
+        foreach (GameObject faceType in faces)
+        {
+            faceType.SetActive(false);
+        }
+        face_Angry.SetActive(true);
+
+        yield return new WaitForSeconds(.1f);
+
+    }
+
+
+
+}
---
+++ Progeny/Assets/Scripts/MicrophoneManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class MicrophoneManager : MonoBehaviour
+{
+    int frequency = 44100;
+
+    AudioSource audioSource;
+    string deviceName;
+    bool isCapturingAudio;
+    DictationRecognizer dictationRecognizer;
+
+    public string youSaid;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        foreach (string device in Microphone.devices)
+        {
+            deviceName = device;
+            Debug.Log("Device detected: " + device);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && Microphone.devices.Length > 0)
+        {
+            if (!isCapturingAudio)
+            {
+                StartCapturingAudio();
+            }
+            else
+            {
+                StopCapturingAudio();
+            }
+        }
+    }
+
+
+    // begin capturing audio from microphone
+    void StartCapturingAudio()
+    {
+        isCapturingAudio = true;
+        audioSource.clip = Microphone.Start(deviceName, true, 30, frequency);
+        audioSource.loop = true;
+        audioSource.Play();
+
+        dictationRecognizer = new DictationRecognizer();
+        dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+        dictationRecognizer.DictationError += DictationRecognizer_DictationError;
+        dictationRecognizer.Start();
+    }
+
+    private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
+    {
+        youSaid = text;
+    }
+
+    private void DictationRecognizer_DictationError(string error, int hresult)
+    {
+        Debug.Log("Dictation Error: " + error);
+    }
+
+    // stop capturing audio from microphone
+    void StopCapturingAudio()
+    {
+        isCapturingAudio = false;
+        Microphone.End(deviceName);
+        dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+        dictationRecognizer.DictationError -= DictationRecognizer_DictationError;
+        dictationRecognizer.Dispose();
+    }
+}
